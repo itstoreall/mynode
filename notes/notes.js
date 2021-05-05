12cfe8604556e310ea6103b5f0804c8a3639b013
Serhii Stanislav <serhiistanislav@gmail.com>
2021-05-05 12:57:01 +0300

    adds info about cookies to the notes

--- notes/notes.js
+++ notes/notes.js
@@ -51,17 +51,56 @@
 mkdir myapp (create folder)
 node -v (check the version of the node.js)
 npx eslint --init (ESLint)
-npm i commander -E (парсит строку запроса)
 npm i @babel/core -D
 npm i @babel/eslint-parser -D
 npm i @babel/plugin-proposal-class-properties -D
 npm i @babel/plugin-proposal-private-methods -D
 npm i @babel/plugin-syntax-top-level-await -D
-npm i ejs -E 
+npm i (Устанавливает модули после Fork перед работой)
+npm i commander -E (парсит строку запроса)
 npm i cors
+npm i ejs -E 
+npm i express ejs (EJS)
 npm i joi
-npm i (Устанавливает модули после Fork перед радотой)
 npm init -y (creates an empty package.json)
 npm install --save-dev nodemon 
+npx express-generator --view=ejs simple-express
+
+------------------------------------------- */
+
+/* ===========================================
+** Cookies
+
+req.cookies (доступ к обычным неподписанным cookie)
+req.cookies['имя куки'] (прочитать обычную куку)
+req.signedCookies['имя куки'] (прочитать подписанную куку)
+res.clearCookie (очищение куков - передать имя куки)
+
+При установке cookie-файла можно указать опции:
+
+- httpOnly - значение true говорит, что cookie-файл 
+изменяется только сервером. Это предотвращает 
+XSS-атаки от JavaScript со стороны клиента
+
+- path - путь, действия данного cookie-файла. 
+По умолчанию путь /. Распространяется на 
+все страницы сайта
+
+- domain - привязывает cookie-файлы к конкретным 
+поддоменам. Устанавливается cookie-файл только 
+для домена на котором работает ваш сервер
+
+- maxAge - определяет, сколько времени в 
+миллисекундах клиент должен хранить cookie-файл 
+до его удаления. Если опция не указана, cookie-файл 
+будет удален при закрытии браузера
+
+- secure - работа с cookie-файлами должна проводиться 
+только через защищенное (HTTPS) соединение
+
+- signed - установаем в true, чтобы подписать данный 
+cookie-файл. После этого он становится доступным в 
+res.signedCookies вместо res.cookies. Поддельные 
+cookie-файлы будут не приняты сервером
 
 ------------------------------------------- */
